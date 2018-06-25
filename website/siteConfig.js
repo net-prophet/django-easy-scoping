3429a07db08cde31d0b3c2abb8acc5aaa6dbc79d
Robert Wells <wellsroberte@gmail.com>
2018-06-25 11:34:06 -0700

    working on circleci

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'Django Easy Scoping' /* title for your website */,
   tagline: 'Scopes, Aggregate Functions, Querysets made easy.',
-  url: 'https://netprophet.tech' /* your website url */,
-  baseUrl: '/' /* base url for your project */,
+  url: 'https://net-prophet.github.io' /* your website url */,
+  baseUrl: '/django-easy-scoping/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
   projectName: 'django-easy-scoping',
-  organizationName: 'NetProphet',
+  organizationName: 'net-prophet',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
@@ -45,7 +45,7 @@ const siteConfig = {
   ],
 
   // If you have users set above, you add it here:
-  users,
+  //users,
 
   /* path to images for header/footer */
   headerIcon: 'img/NPlogo.svg',
