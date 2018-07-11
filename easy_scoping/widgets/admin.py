e07a5817d6cf6f9acdd1067e39989dd3c5ca3e69
Robert Wells <wellsroberte@gmail.com>
2018-07-11 09:55:50 -0700

    Pushing changes to widgets and making better example test data

--- easy_scoping/widgets/admin.py
+++ easy_scoping/widgets/admin.py
@@ -4,8 +4,8 @@ from .models import Widget
 
 class WidgetAdmin(admin.ModelAdmin):
     model = Widget
-    list_display = ('name', 'color', 'size', 'shape', 'used_on',)
-    list_filter = ('color', 'size', 'shape', 'used_on')
+    list_display = ('name', 'color', 'size', 'shape', 'cost')
+    list_filter = ('color', 'size', 'shape', 'cost')
 
 
 admin.site.register(Widget, WidgetAdmin)
--- easy_scoping/widgets/models.py
+++ easy_scoping/widgets/models.py
@@ -2,15 +2,17 @@ from django.db import models
 from .options import COLORS, SIZES, SHAPES
 from DjangoEasyScoping.ScopingMixin import ScopingMixin, ScopingQuerySet
 from django.db.models import Count, Case, When
-import datetime
 
 
 class Widget(ScopingMixin, models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=30, blank=True)
     color = models.CharField(choices=COLORS, max_length=30)
     size = models.CharField(choices=SIZES, max_length=30)
     shape = models.CharField(choices=SHAPES, max_length=30)
-    used_on = models.DateField(default=datetime.date.today)
+    cost = models.DecimalField(max_digits=10,
+                               decimal_places=2,
+                               default=0,
+                               blank=True)
 
     objects = ScopingQuerySet.as_manager()
 
@@ -26,38 +28,50 @@ class Widget(ScopingMixin, models.Model):
     def get_shape(self):
         return self.shape
 
-    def get_used_on(self):
-        return self.used_on
+    def get_cost(self):
+        return self.cost
 
+    def save(self, *args, **kwargs):
+        if self.name == '':
+            self.name = '%s.%s.%s'%(self.color, self.size, self.shape)
+
+        color_sum = sum([ord(x) for x in self.color])
+        size_sum = sum([ord(x) for x in self.size])
+        shape_sum = sum([ord(x) for x in self.shape])
+        self.cost = color_sum + size_sum + shape_sum
+        super(Widget, self).save(*args, **kwargs)
 
 # Basic scopes for testing filtering
-Widget.register_scope('basic_query_widget', lambda qs: qs.filter(color='blue',
-                                                                 size='small',
-                                                                 shape='circle'))
-Widget.register_scope('blue', lambda qs: qs.filter(color='blue'))
-Widget.register_scope('small', lambda qs: qs.filter(size='small'))
-Widget.register_scope('circle', lambda qs: qs.filter(shape='circle'))
-Widget.register_scope('before_y2k',
-             lambda qs: qs.filter(used_on__lte=datetime.date(2000, 1, 1)))
-Widget.register_scope('after_y2k',
-             lambda qs: qs.filter(used_on__gte=datetime.date(2000, 1, 1)))
+Widget.register_scope('basic_query_widget',
+                      lambda qs: qs.filter(color='blue',
+                                           size='small',
+                                           shape='circle'))
+Widget.register_scope('blue',
+                      lambda qs: qs.filter(color='blue'))
+Widget.register_scope('small',
+                      lambda qs: qs.filter(size='small'))
+Widget.register_scope('circle',
+                      lambda qs: qs.filter(shape='circle'))
 
 # Basic scopes for testing excluding
-Widget.register_scope('not_basic_query_widget', lambda qs: qs.exclude(color='blue',
-                                                       size='small',
-                                                       shape='circle'))
-Widget.register_scope('not_blue', lambda qs: qs.exclude(color='blue'))
-Widget.register_scope('not_small', lambda qs: qs.exclude(size='small'))
-Widget.register_scope('not_circle', lambda qs: qs.exclude(shape='circle'))
-Widget.register_scope('not_before_y2k',
-             lambda qs: qs.exclude(used_on__lte=datetime.date(2000, 1, 1)))
-Widget.register_scope('not_after_y2k',
-             lambda qs: qs.exclude(used_on__gte=datetime.date(2000, 1, 1)))
+Widget.register_scope('not_basic_query_widget',
+                      lambda qs: qs.exclude(color='blue',
+                                            size='small',
+                                            shape='circle'))
+Widget.register_scope('not_blue',
+                      lambda qs: qs.exclude(color='blue'))
+Widget.register_scope('not_small',
+                      lambda qs: qs.exclude(size='small'))
+Widget.register_scope('not_circle',
+                      lambda qs: qs.exclude(shape='circle'))
 
 # Scope takes argument
-Widget.register_scope('take_args', lambda qs, i: qs.filter(color=i))
-Widget.register_scope('take_more_args', lambda qs, i, r: qs.filter(color=i, size=r))
-Widget.register_scope('take_kwargs', lambda qs, **kwargs: qs.filter(**kwargs))
+Widget.register_scope('take_args',
+                      lambda qs, i: qs.filter(color=i))
+Widget.register_scope('take_more_args',
+                      lambda qs, i, r: qs.filter(color=i, size=r))
+Widget.register_scope('take_kwargs',
+                      lambda qs, **kwargs: qs.filter(**kwargs))
 
 # Custom aggregate functions
 Widget.register_aggregate('num_blue',
@@ -69,6 +83,5 @@ Widget.register_aggregate('num_blue_small',
                           color='blue', size='small'))))['ret'])
 
 Widget.register_aggregate('num_kwargs',
-                          lambda qs, **kwargs: qs.aggregate( \
-                          ret=Count(Case(When(then=1,
-                          **kwargs))))['ret'])
+                          lambda qs, **kwargs: qs.aggregate(
+                          ret=Count(Case(When(then=1, **kwargs))))['ret'])
